8b1bd180dad3c680ee184582b5185caefeb9f43f
Muhammed Rede Maklad <mohammedmaklad97@gmail.com>
2024-07-16 16:40:27 +0300

    toggle Open Folder

---
+++ src/components/ArrowComponent.tsx
@@ -0,0 +1,14 @@
+// import ArrowIcon from "./SVG/ArrowIcon";
+// interface IProps {}
+
+// const ArrowComponent = ({}: IProps) => {
+//   return (
+//     <>
+//       <span className="cursor-pointer" onClick={}>
+//         <ArrowIcon />
+//       </span>
+//     </>
+//   );
+// };
+
+// export default ArrowComponent;
--- src/components/RecursiveComponent.tsx
+++ src/components/RecursiveComponent.tsx
@@ -2,29 +2,50 @@ import { IFile } from "../interface";
 import FileIcon from "./SVG/FileIcon";
 import FolderIcon from "./SVG/FolderIcon";
 import ArrowIcon from "./SVG/ArrowIcon";
+import { useState } from "react";
+import OpenFolderIcon from "./SVG/OpenFolderIcon";
 
 interface IProps {
   fileTree: IFile;
+  // isOpen: boolean;
+  // setIsOpen: (open: boolean) => void;
 }
 
 const RecursiveComponent = ({
-  fileTree: { name, type, children, content },
-}: IProps) => {
+  fileTree: { name, type, children },
+}: // isOpen,
+// setIsOpen,
+IProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  // Handlers
+  const toggleFolder = () => setIsOpen((prev) => !prev);
+
   return (
     <>
-      <div className="mb-1 ml-2">
+      <div className="mb-1 ml-2 cursor-pointer">
         <div className="flex items-center space-x-2 mb-1">
-          <span>
-            <ArrowIcon />
-          </span>
-          <span>{type === "file" ? <FileIcon /> : <FolderIcon />}</span>
+          {type === "folder" ? (
+            <div onClick={toggleFolder} className="flex space-x-2 items-center">
+              <span style={isOpen ? { rotate: "90deg" } : {}}>
+                <ArrowIcon />
+              </span>
+              <span>{isOpen ? <OpenFolderIcon /> : <FolderIcon />}</span>
+            </div>
+          ) : (
+            <span className="inline-block">
+              <FileIcon />
+            </span>
+          )}
           <span>{name}</span>
         </div>
-        {children &&
-          children.length > 0 &&
-          children.map((child) => (
-            <RecursiveComponent key={child.name} fileTree={child} />
-          ))}
+        {children && isOpen && (
+          <div>
+            {children.map((child, idx) => (
+              <RecursiveComponent fileTree={child} key={idx} />
+            ))}
+          </div>
+        )}
         {/* {content && <div>{content}</div>} */}
       </div>
     </>
